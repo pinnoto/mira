47cd1af17658ebde4a79f1d8563357f96f5a4a22
Troplo <troplo@troplo.com>
2021-12-20 18:31:15 +1100

    Remove sass and sandbox iframe

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import './assets/style.css';
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
